89363ef5c39b59c61b046ec7e30c9dcb4d58f534
Eva Bacas <ENB30@pitt.edu>
2021-09-03 10:56:32 -0400

    first commit

---
+++ afni_qwarp.sh
@@ -0,0 +1,39 @@
+# trying to make AFNI not whine about lack of display
+echo "creating xvfb display..."
+Xvfb :88 &
+export DISPLAY=:88
+
+# load filename and optional parameters
+echo "loading filename and optional parameters..."
+t1=`jq -r '.t1' config.json`
+optional_params=`jq -r '.optional_params' config.json`
+
+3dQwarp -allineate -prefix sub-0202_T1wQ -blur 0 3 -base /usr/local/pkg/afni/TT_152_2009c+tlrc.BRIK.gz 
+-source sub-0202_T1w_ss.nii.gz -minpatch 7 -Qfinal -workhard
+
+# following creates an output filename w a suffix
+# rather than AFNI's default prefix
+# t1_fn=$(echo $t1 | cut -d "." -f 1 | rev | cut -d "/" -f 1 | rev)
+output_fn=t1_ss.nii.gz
+
+# create new directory skullStrip for t1w file and output
+echo "creating skullStrip folder..."
+mkdir -p skullStrip
+
+# move file to new directory skullStrip
+install $t1 skullStrip/t1.nii.gz
+
+# run 3dSkullStrip
+echo "running 3dSkullStrip..."
+if [ "$optional_params" != "null" ]; then
+    3dSkullStrip -input ./skullStrip/t1.nii.gz -prefix ${output_fn} ${optional_params}
+else
+    3dSkullStrip -input ./skullStrip/t1.nii.gz -prefix ${output_fn}
+fi
+
+# move output to output folder
+echo "creating output folder..."
+mkdir -p output
+cp t1_ss.nii.gz output/t1.nii.gz
+
+echo "process complete"
