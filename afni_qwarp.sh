5427afb33aee399b798413952719226ce4a306d7
Eva Bacas <ENB30@pitt.edu>
2021-09-08 11:51:05 -0400

    modified

--- afni_qwarp.sh
+++ afni_qwarp.sh
@@ -4,38 +4,36 @@ Xvfb :88 &
 export DISPLAY=:88
 
 # load filename and optional parameters
-
 echo "loading filename and optional parameters..."
 t1=`jq -r '.t1' config.json`
 optional_params=`jq -r '.optional_params' config.json`
 
 # current there is only ONE option for template
 # this is in here jic we decide to add more
+echo "loading template..."
 template=`jq -r '.template' config.json`
 
 # following creates an output filename w a suffix
 output_fn=t1_qwarp.nii.gz
 
-# create new directory skullStrip for t1w file and output
-echo "creating skullStrip folder..."
-mkdir -p skullStrip
+# create new directory QWarp for t1w file and output
+echo "creating QWarp folder..."
+mkdir -p QWarp
 
 # move file to new directory skullStrip
-install $t1 skullStrip/t1.nii.gz
+install $t1 QWarp/t1.nii.gz
 
-# run 3dSkullStrip
-echo "running 3dSkullStrip..."
+# run 3dQwarp
+echo "running 3dQwarp..."
 if [ "$optional_params" != "null" ]; then
-    3dSkullStrip -input ./skullStrip/t1.nii.gz -prefix ${output_fn} ${optional_params}
+    3dQwarp -source ./QWarp/t1.nii.gz -base $template -prefix ${output_fn} ${optional_params}
 else
-    3dSkullStrip -input ./skullStrip/t1.nii.gz -prefix ${output_fn}
+    3dQwarp -source ./QWarp/t1.nii.gz -base $template -prefix ${output_fn}
 fi
-
-3dQwarp -allineate -prefix sub-0202_T1wQ -blur 0 3 -base $template -source $t1 -minpatch 7 -Qfinal -workhard
 
 # move output to output folder
 echo "creating output folder..."
 mkdir -p output
-cp t1_ss.nii.gz output/t1.nii.gz
+cp t1_qwarp.nii.gz output/t1.nii.gz
 
 echo "process complete"
